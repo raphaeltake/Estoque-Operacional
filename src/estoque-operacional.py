51b13ed362e92827a4e5a0a364a6e0444c940a93
raphaeltake <raph.takeuchi@gmail.com>
2025-07-09 00:04:32 -0300

    criação da função que irá criar o arquivo de divergencias

--- src/estoque-operacional.py
+++ src/estoque-operacional.py
@@ -15,8 +15,10 @@ def ler_arquivo_vendas(arq_vendas, produtos):
   linha = 1
   while conteudo != "":
     codigo, qtde_vendida, situacao_venda, canal_de_venda = conteudo.split(';')
-    if situacao_venda in codigos_divergentes or codigo not in produtos:
-      vendas_divergentes[linha] = [codigo, qtde_vendida, situacao_venda, canal_de_venda]
+    if situacao_venda in codigos_divergentes:
+      vendas_divergentes[linha] = situacao_venda
+    elif codigo not in produtos:
+      vendas_divergentes[linha] = codigo
     else:
       if codigo in lista_de_vendas:
         total_vendas = lista_de_vendas[codigo][0] + int(qtde_vendida)
@@ -38,7 +40,7 @@ def calcular_transferencia(produtos, vendas):
   criar_arquivo_transferencia(dados_transferencia)
 
 def criar_arquivo_transferencia(dados_transferencia):
-  with open('TRANSFERE.txt', 'w') as arquivo:
+  with open('TRANSFERE.txt', 'w', encoding='utf-8') as arquivo:
     dados_ordenados = dict(sorted(dados_transferencia.items()))
     arquivo.write('Necessidade de Transferência Armazém para CO\n')
     arquivo.write(f'{'produtos':<12}{'QtCO':<12}{'QtMin':<12}{'QtVendas':<12}{'Estq.após Vendas':<20}{'Necess.':<12}{'Transf.de Arm p/ CO':<12}\n')
@@ -47,13 +49,25 @@ def criar_arquivo_transferencia(dados_transferencia):
       texto = f'{codigo:<10} {qtde_CO:>5} {minimo:>12} {total_vendas:>14} {estoque_apos_venda:>20} {necessidade_reposicao:>10} {transferencia:>23}\n'
       arquivo.write(texto)
 
+def criar_relatorio_divergentes(divergentes):
+  with open('DIVERGENCIAS.txt', 'w', encoding='utf-8') as arquivo:
+    for linha, item in divergentes.items():
+      if len(item) == 5:
+        texto = f'Código de Produto não encontrado {item}'
+      else:
+        match item:
+          case '135': texto = 'Venda Cancelada'
+          case '190': texto = 'Venda não finalizada'
+          case '999': texto = 'Erro desconhecido. Acionar equipe de TI.'
+      arquivo.write(f'Linha {linha} - {texto}\n')
+
 def main():
   arq_produtos, arq_vendas = 'Casos-de-teste/c1_produtos.txt', 'Casos-de-teste/c1_vendas.txt'
-  with open(arq_produtos, 'r') as arquivo:
+  with open(arq_produtos, 'r', encoding='utf-8') as arquivo:
     produtos = ler_arquivo_produtos(arquivo)
   
-  with open(arq_vendas, 'r') as arquivo:
+  with open(arq_vendas, 'r', encoding='utf-8') as arquivo:
     divergentes, vendas = ler_arquivo_vendas(arquivo, produtos)
   calcular_transferencia(produtos, vendas)
-
+  criar_relatorio_divergentes(divergentes)
 main()
